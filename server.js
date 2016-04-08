3d0ac4cd13bd78a19389a314097ce4181bf6a6f9
Jacob Friis Saxberg <jacob@saxberg.dk>
2016-04-08 23:13:03 +0200

    optim

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const urlParse = require('url').parse;
 const Port = process.env.PORT || 3000;
 
 function defaultResponse (res) {
-  res.end('?');
+  res.end();
 }
 
 function handler (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
   res.setHeader('Access-Control-Allow-Headers', 'accept, origin, content-type');
   switch (req.method) {
     case 'GET':
       const query = urlParse(req.url, true).query;
-      if (!query.url) return;
+      if (!query.url) return defaultResponse(res);
       const oauth = {
         consumer_key: process.env.consumer,
         consumer_secret: process.env.consumer_secret,
