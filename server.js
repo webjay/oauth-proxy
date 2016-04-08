b505b3b8b9a4f22256d1d57995ab7dbbe323769c
Jacob Friis Saxberg <jacob@saxberg.dk>
2016-04-08 23:03:29 +0200

    launch

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const request = require('request');
+const createServer = require('http').createServer;
+const urlParse = require('url').parse;
+
+const Port = process.env.PORT || 3000;
+
+function defaultResponse (res) {
+  res.end('?');
+}
+
+function handler (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'accept, origin, content-type');
+  switch (req.method) {
+    case 'GET':
+      const query = urlParse(req.url, true).query;
+      if (!query.url) return;
+      const oauth = {
+        consumer_key: process.env.consumer,
+        consumer_secret: process.env.consumer_secret,
+        token: process.env.token,
+        token_secret: process.env.token_secret
+      };
+      request.get({
+        url: query.url,
+        oauth: oauth,
+        json: true
+      }).pipe(res);
+      break;
+    default:
+      defaultResponse(res);
+      break;
+  }
+}
+
+const server = createServer(handler);
+
+server.listen(Port, _ => {
+  console.log('HTTP server listening on port', Port);
+});
+
+process.on('SIGTERM', _ => {
+  server.close();
+});
